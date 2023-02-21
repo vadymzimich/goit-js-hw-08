9d3b1459a97e3c27cb8e7d767d51dc216aeabab6
Vadym Zimich <tunego97@gmail.com>
2023-02-21 02:51:54 +0200

    fix bugs

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,10 +4,6 @@ import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
 
-import SimpleLightbox from 'simplelightbox';
-
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
 const gallery = document.querySelector('.gallery');
 const items = [];
 
